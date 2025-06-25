588e6f80e245205214241a75baad69533b6288e7
igor4022 <tic80padraolua@gmail.com>
2025-06-25 00:23:12 -0300

    atulização

---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}
+
+/*s
+        ___   _
+        /   '-/  ]           ,,,
+        [  ]=@==============#   ]
+        /_______--]          ,,,    IGOR...(-_-)
+
+*/
