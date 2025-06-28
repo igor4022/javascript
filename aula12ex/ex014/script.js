7887ad573493f47a5537a380438eaf4fbda99b49
igor4022 <tic80padraolua@gmail.com>
2025-06-28 09:24:14 -0300

    blz

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-window.alert(`Olá`)
+function carregar() {
+    var msg = window.document.getElementById(`msg`)
+    var img = window.document.getElementById(`imagem`)
+    var data= new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+
+    msg.innerHTML = `agora são ${hora} horas e ${minuto} minutos`
+
+    if (hora >= 0 && hora < 12) {
+        // Bom dia flor do dia!
+        img.src = 'imagem_dia.png.png'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde bela adoremecida!
+        img.src = 'imagem_tarde.png.png'
+        document.body.style.background = `#e8ff71`
+    } else {
+        // I sé ta vivo...QUE DIZER!....BOa noite dorminhoco
+        img.src = 'imagem_noite.png.png'
+        document.body.style.background = `#3a2e2e`
+    }
+}
