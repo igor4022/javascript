614f3850197a8032a14d02700435990117a238ed
igor4022 <tic80padraolua@gmail.com>
2025-06-29 13:45:40 -0300

    atulizações

--- ex016/script.js
+++ ex016/script.js
@@ -1,1 +1,18 @@
-window.alert(`Olá`)
+function resultado() {
+
+    var inc = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var pas = window.document.getElementById('passo')
+    var i = Number(inc.value)
+    var f = Number(fim.value)
+    var p = Number(pas.value)
+    var cal = window.document.getElementById('calculando')
+    var m = i, f, p
+    var r = window.document.getElementById('resultado')
+
+    if (cal + m ) {
+        for (var c = i;c <= f;c + p ) {
+            r.innerHTML + `${c}`
+        }
+    }
+}
