7d4121ef5b74e19c371be0c9ad20df334d0b057b
igor4022 <tic80padraolua@gmail.com>
2025-07-07 00:59:04 -0300

    atulização

--- aula17ex/modelo/script.js
+++ aula17ex/modelo/script.js
@@ -1,1 +1,32 @@
-window.alert(`Olá`)
+let clic = window.document.getElementById('digite')
+let res = window.document.getElementById('conta')
+let valores = []
+
+let fim = window.document.getElementById('final')
+
+function clicar() {
+    if (clic.value.lengeth == 0) {
+        window.alert('[ERRO]')
+    } else {
+        
+        let item = document.createElement('option')
+
+        valores.push(Number(clic.value))
+        item.text = `Valor ${clic.value} adicionado`
+        res.appendChild(item)
+    }
+}
+
+clic.value = ''
+clic.focus()
+
+function resultado() {
+    if (valores.lenght == 0) {
+        window.alert('[ERRO]')
+    } else {
+        let tot = valores.lengeth
+        
+        fim.innerHTML = ''
+        fim.innerHTML += `${tot}`
+    }
+}
