f72b5659bfb882b5391f738a21765b71d46d6cd2
igor4022 <tic80padraolua@gmail.com>
2025-10-10 00:05:08 -0300

    blz

---
+++ Cartas_Jogo/docuento.js
@@ -0,0 +1,39 @@
+const cartas = document.querySelectorAll(".carta")
+const locals = document.querySelectorAll(".local")
+const mesas = document.querySelectorAll(".mesa")
+
+let movimento 
+
+const dragStart = (event) => {
+    movimento = event.target
+    console.log(movimento)
+}
+
+const dragOver = (event) => {
+    event.preventDefault()
+}
+
+const drop = ({ target }) => {
+    target.append(movimento)
+}
+
+/*Colocação de campo de batalha / FUNÇÕES de CAMPO*/
+
+mesas.forEach((cas) => {
+
+    cas.addEventListener("drop", drop)
+    cas.addEventListener("dragover", dragOver)
+})
+/*Colocação da mesa do jogador / FUNÇÔES de MESA*/
+
+locals.forEach((cal) => {
+
+    cal.addEventListener("drop", drop)
+    cal.addEventListener("dragover", dragOver)
+})
+/*colocação das cartas / FUNÇÔES das CARTAS */
+
+cartas.forEach((card) => {
+
+    card.addEventListener("dragstart", dragStart)
+})
